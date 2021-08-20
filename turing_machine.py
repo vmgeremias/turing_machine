0789a987bc5f7d06868d99cbd19b00a6bcfc6174
Vinicius Michelon Geremias <39409884+vmgeremias@users.noreply.github.com>
2021-08-20 16:29:42 -0300

    Add files via upload

--- turing_machine.py
+++ turing_machine.py
@@ -1,115 +1,113 @@
 # Começo o programa lendo  o arquivo de entrada e retirando o \n
-print("Qual o nome do arquivo para leitura")
-file = open(input(), "r")
+file = open("entrada.in", "r")
 linha = file.readline().replace("\n", "")
 # A variavel de comandos será onde fica todas as instrucoes da nossa maquina de Turing
-# A variaveis de estados_iniciados será utilizada para demarcar os novos estados e colocar nos comandos uma instrucao para caso o ponteiro
-# chegar no simbolo de limite a esquerda, voltar para a direita.
 comandos = []
-estados_iniciados = []
+estados_estacionarios = 0
 
-# Leio a primeira linha da entrada para verificar o tipo de máquina
-if (linha == ";S"):
-    # Adiciono o novo tipo da máquina
-    comandos.append([";I"])
-    # Adiciono instrucoes para colocar o simbolo de limite a esquerda e modifico o estado inicial para um estado new0
-    # Todos os estados na fita de estrada irão possui "new" na frente. Exemplo: 1 -> new1, 2 -> new2
-    comandos.append(["0", "*", "*", "l", "firstIcon"])
-    comandos.append(["firstIcon", "_", "¢", "r", "new0"])
-    comandos.append(["firstIcon", "*", "*", "*", "new0"])
+# Estados necessarios caso a palavra seja vazia e não escrever sem querer um branco
+# o verdadeiro estado inicial (normalmente sendo 0) é 0init
+comandos.append(["0", "_", '£', "l", "firstIcon"])
+comandos.append(["0", "0", '0', "l", "firstIcon"])
+comandos.append(["0", "1", '1', "l", "firstIcon"])
+comandos.append(["firstIcon", "_", '¢', "r", "step"])
+comandos.append(["step", "_", '£', "l", "returnFirst"])
+comandos.append(["step", "0", '0', "r", "step"])
+comandos.append(["step", "1", '1', "r", "step"])
+comandos.append(["step", "£", '£', "r", "step"])
+comandos.append(["returnFirst", "£", '£', "l", "returnFirst"])
+comandos.append(["returnFirst", "0", '0', "l", "returnFirst"])
+comandos.append(["returnFirst", "1", '1', "l", "returnFirst"])
+comandos.append(["returnFirst", "¢", '£', "r", "0init"])
+
+# Iteração por linha
+while(linha != ""):
+    # Verifico se a linha é um comentário e ignoro
+    if (linha[0] == ";" or linha[0] == "\n"):
+        linha = file.readline()
+        continue
+
+    # Realizo a divisão de cada segmento do comando e retiro o \n do final
+    linha = linha.split(' ')
+    linha[4] = linha[4].replace("\n", "")
+
+    # Caso for o estado 0 troco por 0init
+    linha[0] = "0init" if linha[0] == "0" else linha[0]
+    linha[4] = "0init" if linha[4] == "0" else linha[4]
+
+    # Faço a verificação para ver se é um estado estacionario
+    # sempre preciso verificar se o comando escreve _
+    # caso sim, mude para o simbolo £
+    # Em alguns casos faço dois comandos para branco
+    if (linha[3] == "*"):
+        if (linha[1] == "_" and linha[2] == "_"):
+            comandos.append([linha[0], linha[1], '£', "r",
+                             "est"+str(estados_estacionarios)])
+            comandos.append([linha[0], '£', '£', "r",
+                             "est"+str(estados_estacionarios)])
+            comandos.append(["est"+str(estados_estacionarios),
+                             "0", "0", "l", linha[4]])
+            comandos.append(["est"+str(estados_estacionarios),
+                             "1", "1", "l", linha[4]])
+            comandos.append(["est"+str(estados_estacionarios),
+                             "_", "£", "l", linha[4]])
+            comandos.append(["est"+str(estados_estacionarios),
+                             "£", "£", "l", linha[4]])
+        elif (linha[1] == "_"):
+            comandos.append([linha[0], linha[1], linha[2], "r",
+                             "est"+str(estados_estacionarios)])
+            comandos.append([linha[0], "£", linha[2], "r",
+                             "est"+str(estados_estacionarios)])
+            comandos.append(["est"+str(estados_estacionarios),
+                             "0", "0", "l", linha[4]])
+            comandos.append(["est"+str(estados_estacionarios),
+                             "1", "1", "l", linha[4]])
+            comandos.append(["est"+str(estados_estacionarios),
+                             "_", "£", "l", linha[4]])
+            comandos.append(["est"+str(estados_estacionarios),
+                             "£", "£", "l", linha[4]])
+        elif(linha[2] == "_"):
+            comandos.append([linha[0], linha[1], '£', "r",
+                             "est"+str(estados_estacionarios)])
+            comandos.append(["est"+str(estados_estacionarios),
+                             "0", "0", "l", linha[4]])
+            comandos.append(["est"+str(estados_estacionarios),
+                             "1", "1", "l", linha[4]])
+            comandos.append(["est"+str(estados_estacionarios),
+                             "_", "£", "l", linha[4]])
+            comandos.append(["est"+str(estados_estacionarios),
+                             "£", "£", "l", linha[4]])
+        else:
+            comandos.append([linha[0], linha[1], linha[2], "r",
+                             "est"+str(estados_estacionarios)])
+            comandos.append(["est"+str(estados_estacionarios),
+                             "0", "0", "l", linha[4]])
+            comandos.append(["est"+str(estados_estacionarios),
+                             "1", "1", "l", linha[4]])
+            comandos.append(["est"+str(estados_estacionarios),
+                             "_", "£", "l", linha[4]])
+            comandos.append(["est"+str(estados_estacionarios),
+                             "£", "£", "l", linha[4]])
+        estados_estacionarios += 1
+    else:
+        if (linha[1] == "_" and linha[2] == "_"):
+            comandos.append([linha[0], linha[1], '£', linha[3], linha[4]])
+            comandos.append([linha[0], '£', '£', linha[3], linha[4]])
+        elif (linha[1] == "_"):
+            comandos.append([linha[0], linha[1], linha[2], linha[3], linha[4]])
+            comandos.append([linha[0], '£', linha[2], linha[3], linha[4]])
+        elif(linha[2] == "_"):
+            comandos.append([linha[0], linha[1], '£', linha[3], linha[4]])
+        else:
+            comandos.append([linha[0], linha[1], linha[2], linha[3], linha[4]])
 
     linha = file.readline()
-    # Iteração por linha
-    while(linha != ""):
-        # Verifico se a linha é um comentário e ignoro
-        if (linha[0] == ";"):
-            comandos.append([linha.replace("\n", "")])
-            linha = file.readline()
-            continue
+file.close()
 
-        # realizo a divisão de cada segmento do comando e retiro o \n do final
-        linha = linha.split(' ')
-        linha[4] = linha[4].replace("\n", "")
+# Por fim coloco cada instrucao obtida no arquivo de saida
+file = open("saida.out", "w")
+for linha in comandos:
+    file.write(
+        ' '.join([linha[0], linha[1], linha[2], linha[3], linha[4]+"\n"]))
 
-        # Caso o estado não seja de parada (halt-reject ou halt-accept), adiciono o "new" na frente
-        # e coloco a instrucao na variavel
-        if (linha[4] != "halt-reject" and linha[4] != "halt-accept"):
-            comandos.append(["new"+linha[0], linha[1], linha[2],
-                             linha[3], "new"+linha[4]])
-        else:
-            comandos.append(["new"+linha[0], linha[1], linha[2],
-                             linha[3], linha[4]])
-
-        # Verifico se o estado já foi computado pelo menos uma vez, caso não coloco na lista e adiciono a instrucao
-        # para caso chegue no limite pela esquerda nesse estado, ir para a direita
-        if ("new"+linha[0] not in estados_iniciados):
-            estados_iniciados.append("new"+linha[0])
-            comandos.append(["new"+linha[0], "¢", "¢", "r", "new"+linha[0]])
-        linha = file.readline()
-    file.close()
-
-    # Por fim coloco cada instrucao obtida no arquivo de saida
-    file = open("out.txt", "w")
-    for linha in comandos:
-        # Tratamento para comentários
-        if (linha[0][0] == ";"):
-            file.write(linha[0]+"\n")
-        else:
-            file.write(
-                ' '.join([linha[0], linha[1], linha[2], linha[3], linha[4]+"\n"]))
-
-    file.close()
-elif (linha == ";I"):
-    # flag para saber se o estado first icon
-    flag_firstIcon = 0
-    # Adiciono o novo tipo da máquina
-    comandos.append([";S"])
-    linha = file.readline()
-    # Iteração por linha
-    while(linha != ""):
-        # Verifico se a linha é um comentário e ignoro
-        if (linha[0] == ";"):
-            comandos.append([linha.replace("\n", "")])
-            linha = file.readline()
-            continue
-
-        # realizo a divisão de cada segmento do comando e retiro o \n do final
-        linha = linha.split(' ')
-        linha[4] = linha[4].replace("\n", "")
-
-        # Removo os comandos no qual possuem o simbolo de parada
-        if ((linha[1] == "¢" and linha[2] == "¢") or linha[0] == "0"):
-            linha = file.readline()
-            continue
-
-        # Caso o estado não seja de parada (halt-reject ou halt-accept), adiciono o "new" na frente
-        # e coloco a instrucao na variavel
-        if (linha[4] != "halt-reject" and linha[4] != "halt-accept" and "firstIcon" not in linha[0]):
-            comandos.append(["new"+linha[0], linha[1], linha[2],
-                             linha[3], "new"+linha[4]])
-        # Fazer a instrucao para o novo caminho do estado inicial
-        elif ("firstIcon" in linha[0] and flag_firstIcon == 0):
-            novoInicio = linha[4]
-            comandos.append(["0", "*", "*", "r", "auxInicio"])
-            comandos.append(["auxInicio", "*", "*", "l", "new"+novoInicio])
-            flag_firstIcon = 1
-        else:
-            comandos.append(["new"+linha[0], linha[1], linha[2],
-                             linha[3], linha[4]])
-
-        linha = file.readline()
-    file.close()
-
-    # Por fim coloco cada instrucao obtida no arquivo de saida
-    file = open("out.txt", "w")
-    for linha in comandos:
-        # Tratamento para comentários
-        if (linha[0][0] == ";"):
-            file.write(linha[0]+"\n")
-        else:
-            file.write(
-                ' '.join([linha[0], linha[1], linha[2], linha[3], linha[4]+"\n"]))
-
-    file.close()
-else:
-    print("Máquina não detectada")
+file.close()
